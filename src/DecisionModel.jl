d8ffe940207a8959f8e8155b0d81925beddb5de8
Topias Terho <topias.terho@aalto.fi>
2022-01-26 12:06:43 +0200

    fix errors

--- src/DecisionModel.jl
+++ src/DecisionModel.jl
@@ -1,21 +1,20 @@
 using JuMP
+using Combinatorics
 
-function decision_variable(model::Model, S::States, d::Node, I_d::Vector{Node}, base_name::String="")
+function decision_variable(model::Model, S::States, d::Node, I_d::Vector{Node}, base_name::String="",A_j::Vector{ConditionalParentInfo})
     # Create decision variables.
     dims = S[[I_d; d]]
     z_d = Array{VariableRef}(undef, dims...)
+    for a in collect(comibnations(A_j))
+        parents <- map(x->x.parent,a)
+        println(parents)
+    end
     for s in paths(dims)
         z_d[s...] = @variable(model, binary=true, base_name=base_name)
     end
     # Constraints to one decision per decision strategy.
     for s_I in paths(S[I_d])
-        #if isempty(P_d)
-            @constraint(model, sum(z_d[s_I..., s_d] for s_d in 1:S[d]) == 1)
-        #else
-        #    for s_h in S[P_d]
-        #        @constraint(model, sum(z_d[s_I..., s_d] for s_d in 1:S[d]) == 1)
-        #    end
-        #end
+        @constraint(model, sum(z_d[s_I..., s_d] for s_d in 1:S[d]) == 1)
     end
     return z_d
 end
