7467c3d789d8c5fbd800901e41549fc9e39d6a2c
Topias Terho <topias.terho@aalto.fi>
2022-03-10 15:24:56 +0200

    fix

--- src/DecisionModel.jl
+++ src/DecisionModel.jl
@@ -261,7 +261,7 @@ function decision_path_constraints(model::Model, S::States, d::Node, I_d::Vector
                     println(s_d_s_k)
                     println(s_d_s_Id)
                     println("--------------------")
-                    @constraint(model, abs(z[s_d_s_k...]- z[s_d_s_Id...]) <= x_x[k])
+                    @constraint(model, abs.(z[s_d_s_k...]- z[s_d_s_Id...]) <= x_x[k])
                 end
             end 
         end
