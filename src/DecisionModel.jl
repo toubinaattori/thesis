714af9f4cb7273142fef89b797770658d0a00469
Topias Terho <topias.terho@aalto.fi>
2022-04-19 14:51:21 +0300

    fix

--- src/DecisionModel.jl
+++ src/DecisionModel.jl
@@ -230,8 +230,8 @@ function augmented_state_constraints(model::Model, S::States, d::Node, I_d::Vect
         s_d_s_Id = paths(dims,indices)
         zero = filter(x -> x[indices] == 0, s_d_s_Id)
         non_zero = filter(x -> x[indices] >= 0, s_d_s_Id)
-        @constraint(model,sum(z[s...] for s in non_zero) <= (count(paths(dims,indices))*x_x[k])))
-        @constraint(model,sum(z[s...] for s in zero) <= (count(paths(dims,indices))*(1-x_x[k]))))
+        @constraint(model,sum(z[s...] for s in non_zero) <= (count(paths(dims,indices))*x_x[k]))
+        @constraint(model,sum(z[s...] for s in zero) <= (count(paths(dims,indices))*(1-x_x[k])))
     end
 end
 
