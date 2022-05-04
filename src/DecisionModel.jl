8497893c69b57e3896143d2f628afb4bc62132ad
Topias Terho <topias.terho@aalto.fi>
2022-05-04 11:44:45 +0300

    fix

--- src/DecisionModel.jl
+++ src/DecisionModel.jl
@@ -45,7 +45,7 @@ function decision_variableA(model::Model, S::States, d::Node, I_d::Vector{Node},
     pop!(dims)
     augmented_paths = Iterators.filter(x -> x ∉ paths(dims), paths(dimensions))
     for s_I in paths(S[I_d])
-        @constraint(model, sum(z_d[s_I..., s_d] + sum(z_d[s..., s_d] for s in augmented_paths) for s_d in 1:S[d])  == prod(dimens[s]^x_x[s] for s in filter(x -> x[2] == d,K)))
+        @constraint(model, sum(z_d[s_I..., s_d] + sum(z_d[s..., s_d] for s in augmented_paths) for s_d in 1:S[d])  == 1))
     end
     return z_d
 end
