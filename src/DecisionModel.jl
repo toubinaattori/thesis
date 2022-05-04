e6cf2870e8ab8ce36f0a42387da2456c7af04e67
Topias Terho <topias.terho@aalto.fi>
2022-05-04 13:59:53 +0300

    fix

--- src/DecisionModel.jl
+++ src/DecisionModel.jl
@@ -140,7 +140,7 @@ function decision_strategy_constraint(model::Model, S::States, d::Node, I_d::Vec
         # paths with (s_d | s_I(d)) information structure
         feasible_paths = filter(s -> s[[I_d; d]] == s_d_s_Id, existing_paths)
         if augmented_states
-            feasible_augmented_paths = filter(s -> all((s_d_s_Id.==s) .| (s .== (dims .+ 1))),augmented_paths)
+            feasible_augmented_paths = Iterators.filter(s -> all((s_d_s_Id.==s) .| (s .== (dims .+ 1))),augmented_paths)
             @constraint(model, sum(get(x_s, s, 0) for s in feasible_paths) <= (z[s_d_s_Id...] + sum(z[s...] for s in feasible_augmented_paths)) * min(length(feasible_paths), theoretical_ub))
         else
             @constraint(model, sum(get(x_s, s, 0) for s in feasible_paths) <= z[s_d_s_Id...] * min(length(feasible_paths), theoretical_ub))
